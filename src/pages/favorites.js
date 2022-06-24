beda7ec2a3636028d013b2bdab8dda2e606e6359
Stanislav <stasdamaratskiy@gmail.com>
2022-06-24 07:51:50 +0300

    f

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -23,15 +23,26 @@ function Favorites() {
               >
                 <Card.Header>
                   <Row>
-                    <Col xs={6}>Header</Col>
-                    <Col xs={6}></Col>
+                    <Col xs={6}>{item.name}</Col>
+                    <Col xs={6} className="text-end">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-x"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                      </svg>
+                    </Col>
                   </Row>
                 </Card.Header>
                 <Card.Body>
-                  <Card.Title> Card Title </Card.Title>
+                  <Card.Title>{item.employer.name}</Card.Title>
+                  <Card.Text>{item.snippet.responsibility}</Card.Text>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    <a href={item.alternate_url}>Подробнее...</a>
                   </Card.Text>
                 </Card.Body>
               </Card>
